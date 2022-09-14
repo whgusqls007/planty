050eb8995fb1cd86bf2e010aa27b8ee0a624264d
hyunspace <33j073@gmail.com>
2022-09-14 17:54:11 +0900

    ✨ BE Plant Modeling & Viewset
    - MySql 연결 후 식물 정보 테이블 생성
    - 식물 사전 ReadOnlyViewset 생성

--- backend/plants/views.py
+++ backend/plants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Plant
+from rest_framework import viewsets, status
+from .serializers import PlantDictSerializer
 
 # Create your views here.
+class PlantDictViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Plant.objects.all()
+    serializer_class = PlantDictSerializer
