5f87e07396af3d93ddff7330756082596cdec13c
hyunspace <at.hyunspace@gmail.com>
2022-09-20 01:20:13 +0900

    ✨ 식물 사전 초기 목록 및 상세 페이지
    - 식물 사전 초기 목록
    - 식물 사전 상세 페이지
    - 식물 사전 검색 기능(-ing)

--- backend/plants/serializers.py
+++ backend/plants/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Plant
 
-class PlantDictSerializer(serializers.ModelSerializer):
+class PlantListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plant
+        fields = [
+            'cntntsno',
+            'cntntssj',
+        ]
+
+class PlantDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plant
         fields = [
