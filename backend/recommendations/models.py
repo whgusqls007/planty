322f52292c537f5e51a6e0eb684c75845574e206
hyunspace <at.hyunspace@gmail.com>
2022-10-02 02:07:17 +0900

    📝 Plants, Recommendations 모델 업데이트
    - verbose_name 추가
    - 각 앱의 불필요한 inspectdb 파일 삭제

--- backend/recommendations/models.py
+++ backend/recommendations/models.py
@@ -2,17 +2,58 @@ from django.db import models
 from django.conf import settings
 
 class UserKeywordCount(models.Model):
-    user_id = models.BigIntegerField(primary_key=True)
-    pet_safe = models.IntegerField(blank=True, null=True)
-    humidify = models.IntegerField(blank=True, null=True)
-    pm_cleaning = models.IntegerField(blank=True, null=True)
-    beginner = models.IntegerField(blank=True, null=True)
-    unscented = models.IntegerField(blank=True, null=True)
-    hydroponics = models.IntegerField(blank=True, null=True)
-    low_growth_demand = models.IntegerField(blank=True, null=True)
-    low_light_demand = models.IntegerField(blank=True, null=True)
-    low_temp = models.IntegerField(blank=True, null=True)
-    air_cleaning = models.IntegerField(blank=True, null=True)
+    user_id = models.BigIntegerField(
+        primary_key=True
+    )
+    pet_safe = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="반려동물 안전성"
+    )
+    humidify = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="가습 기능"
+    )
+    pm_cleaning = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="미세먼지 정화 기능"
+    )
+    beginner = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="초보자용"
+    )
+    unscented = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="무향"
+    )
+    hydroponics = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="수경 재배 가능"
+    )
+    low_growth_demand = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="손쉬운 관리"
+    )
+    low_light_demand = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="적은 빛"
+    )
+    low_temp = models.IntegerField(
+        blank=True, 
+        null=True,
+        verbose_name="낮은 온도"
+    )
+    air_cleaning = models.IntegerField(
+        blank=True, 
+        null=True
+    )
 
     class Meta:
         managed = False
