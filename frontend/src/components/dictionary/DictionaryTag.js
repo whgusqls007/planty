234301cda61daa40feffe53339d4cecb937d194f
Hong Seong Mok <swff07183@gmail.com>
2022-10-04 13:58:43 +0900

    ✨ client 설정 추가
    - client 설정 추가
    
    
    Former-commit-id: 20a878b32dfa4ea702504ede1bf0d9623ddea049

--- frontend/src/components/dictionary/DictionaryTag.js
+++ frontend/src/components/dictionary/DictionaryTag.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { DictionaryTagWrapper } from '../../styles/dictionary/DictionaryComponentStyle';
-import DictionaryTagButton from './DictionaryTagButton';
+import { DictionaryTagButton } from '../../styles/dictionary/DictionaryComponentStyle';
+import { useNavigate } from 'react-router-dom';
 
 const DictionaryTag = ({ tagList }) => {
+  const navigate = useNavigate();
   return (
     <DictionaryTagWrapper>
       {tagList.map((tag, i) => (
-        <DictionaryTagButton tag={tag} key={i} />
+        <DictionaryTagButton
+          tag={tag}
+          key={i}
+          onClick={() => navigate(`/dictionary?filter=${i}`, { replace: true })}
+        >
+          {tag}
+        </DictionaryTagButton>
       ))}
     </DictionaryTagWrapper>
   );
