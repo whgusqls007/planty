b203d0ba80b5e21dc010f6f7ef884b90124c71a6
JangJiseon <jsmeaningood@gmail.com>
2022-10-04 20:53:05 +0900

    ✨ 마이페이지 나의 댓글, 좋아요한 글
    - 마이페이지 나의 댓글, 좋아요한 글 목록 띄우기
    - detail 페이지로 navigate
    
    
    Former-commit-id: 8671339ab6075cd51ee29509d6693c74bd7c8032

--- frontend/src/pages/user/ProfilePage.js
+++ frontend/src/pages/user/ProfilePage.js
@@ -4,7 +4,8 @@ import Container from 'react-bootstrap/esm/Container';
 import { Link, useSearchParams, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import SettingsIcon from '@mui/icons-material/Settings';
-import ProfileListIem from '../../components/user/ProfileListItem';
+import ProfileCommentListItem from '../../components/user/ProfileCommentListItem';
+import ProfileLikeListItem from '../../components/user/ProfileLikeListItem';
 import {
   fetchUserComments,
   fetchUserLikes,
@@ -79,10 +80,8 @@ const ProfilePage = () => {
           </div>
         </div>
         <ProfileWrapper>
-          {/* {profileNum === 1 && <div className="profile1">profile1</div>}
-          {profileNum === 2 && <div className="profile2">profile2</div>}
-          {profileNum === 3 && <div className="profile3">profile3</div>} */}
-          <ProfileListIem />
+          {profileNum === 1 && <ProfileCommentListItem />}
+          {profileNum === 2 && <ProfileLikeListItem />}
         </ProfileWrapper>
       </Wrapper>
     </Container>
