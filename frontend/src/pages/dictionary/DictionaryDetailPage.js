2ba291d0900343cc83800c889f76076f46f733cf
hyunspace <33j073@gmail.com>
2022-10-05 15:24:49 +0900

    ✨ 식물 사전 CSS
    - 식물 사전 검색 창 styling
    - 식물 사전 상세 창 styling
    
    
    Former-commit-id: 943879afc0f42ec9421723bc242f82f781c2e192

--- frontend/src/pages/dictionary/DictionaryDetailPage.js
+++ frontend/src/pages/dictionary/DictionaryDetailPage.js
@@ -10,7 +10,7 @@ import {
   GrowInfoWrapper,
   PlantImg,
 } from '../../styles/dictionary/DictionaryDetailStyle';
-
+import HtmlParser from 'react-html-parser';
 const DictionaryDetailPage = () => {
   const dispatch = useDispatch();
   const { plantId } = useParams();
@@ -36,6 +36,7 @@ const DictionaryDetailPage = () => {
     growth_width,
     growth_height,
     growth_temp,
+    winter_lowest_temp,
     growth_speed,
     humidity,
     blooming_season,
@@ -59,10 +60,32 @@ const DictionaryDetailPage = () => {
             <PlantDescription plant={plant} />
           </div>
           <div className="dictionary-detail-body">
-            <div className="grow-info-div">
-              <GrowInfo growth_temp={growth_temp} />
-              <GrowInfo growth_speed={growth_speed} />
-              <GrowInfo humidity={humidity} />
+            <div className="grow-info-container">
+              <h5>온도와 습도</h5>
+              <hr />
+              <div className="grow-info-div">
+                <GrowInfo growth_temp={growth_temp} />
+                <GrowInfo winter_lowest_temp={winter_lowest_temp} />
+                <GrowInfo humidity={humidity} />
+              </div>
+            </div>
+            <div className="grow-info-container">
+              <h5>물 주는 방법</h5>
+              <hr />
+              <div className="water-info-div">
+                <WaterInfo water_cycle_spring={water_cycle_spring} />
+                <WaterInfo water_cycle_summer={water_cycle_summer} />
+                <WaterInfo water_cycle_autumn={water_cycle_autumn} />
+                <WaterInfo water_cycle_winter={water_cycle_winter} />
+              </div>
+            </div>
+            <div className="grow-info-container">
+              <h5>빛과 위치</h5>
+              <hr />
+              <div className="lightposi-info-div">
+                <PlaceInfo light_demand={light_demand} />
+                <PlaceInfo posting_place={posting_place} />
+              </div>
             </div>
           </div>
         </Wrapper>
@@ -71,29 +94,106 @@ const DictionaryDetailPage = () => {
   );
 };
 
-const GrowInfo = ({ growth_temp, growth_speed, humidity }) => {
+const GrowInfo = ({ growth_temp, winter_lowest_temp, humidity }) => {
   return (
     <GrowInfoWrapper>
       {growth_temp && (
         <>
-          <img src="/assets/img/sun.png" alt="" />
-          {growth_temp}
+          <img src="/assets/img/hot.png" alt="온도계 아이콘" />
+          <div className="growth-info-data">
+            <p className="title-style">적정 온도</p>
+            <p>{growth_temp}</p>
+          </div>
         </>
       )}
-      {growth_speed && (
+      {winter_lowest_temp && (
         <>
-          <img src="/assets/img/ground.png" alt="" />
-          {growth_speed}
+          <img src="/assets/img/cold.png" alt="겨울 온도계 아이콘" />
+          <div className="growth-info-data">
+            <p className="title-style">겨울철 최저 온도</p>
+            <p>{winter_lowest_temp}</p>
+          </div>
         </>
       )}
       {humidity && (
         <>
-          <img src="/assets/img/water.png" alt="" />
-          {humidity}
+          <img src="/assets/img/water.png" alt="습도 아이콘" />
+          <div className="growth-info-data">
+            <p className="title-style">적정 습도</p>
+            <p>{humidity}</p>
+          </div>
         </>
       )}
     </GrowInfoWrapper>
   );
 };
 
+const WaterInfo = ({
+  water_cycle_spring,
+  water_cycle_summer,
+  water_cycle_autumn,
+  water_cycle_winter,
+}) => {
+  return (
+    <GrowInfoWrapper>
+      {water_cycle_spring && (
+        <>
+          <img src="/assets/img/wtr-spring.png" alt="봄 아이콘" />
+          <div className="water-info-data">
+            <p className="title-style">봄</p>
+            <p>{water_cycle_spring}</p>
+          </div>
+        </>
+      )}
+      {water_cycle_summer && (
+        <>
+          <img src="/assets/img/wtr-summer.png" alt="여름 아이콘" />
+          <div className="water-info-data">
+            <p className="title-style">여름</p>
+            <p>{water_cycle_summer}</p>
+          </div>
+        </>
+      )}
+      {water_cycle_autumn && (
+        <>
+          <img src="/assets/img/wtr-autumn.png" alt="가을 아이콘" />
+          <div className="water-info-data">
+            <p className="title-style">가을</p>
+            <p>{water_cycle_autumn}</p>
+          </div>
+        </>
+      )}
+      {water_cycle_winter && (
+        <>
+          <img src="/assets/img/wtr-winter.png" alt="겨울 아이콘" />
+          <div className="water-info-data">
+            <p className="title-style">겨울</p>
+            <p>{water_cycle_winter}</p>
+          </div>
+        </>
+      )}
+    </GrowInfoWrapper>
+  );
+};
+
+const PlaceInfo = ({ light_demand, posting_place }) => {
+  // posting_place = posting_place.replaceAll(',', '&nbsp;');
+  console.log(posting_place && HtmlParser(posting_place.replaceAll(',', '\n')));
+  return (
+    <GrowInfoWrapper>
+      {light_demand && (
+        <>
+          <img src="/assets/img/light.png" alt="빛 아이콘" />
+          {HtmlParser(light_demand.replaceAll('),', ')\n'))}
+        </>
+      )}
+      {posting_place && (
+        <>
+          <img src="/assets/img/location.png" alt="위치 아이콘" />
+          {HtmlParser(posting_place.replaceAll('),', ')\n'))}
+        </>
+      )}
+    </GrowInfoWrapper>
+  );
+};
 export default DictionaryDetailPage;
