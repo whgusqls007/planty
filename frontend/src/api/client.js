bbc49ea3cb798a90bd585033566fefb94846f53b
JangJiseon <jsmeaningood@gmail.com>
2022-10-06 16:29:23 +0900

    🐛 client 주석 해제
    - client 주석 해제
    
    
    Former-commit-id: 3c53759155bf5374295f7c854a41d983c10e3d50

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const client = axios.create({
   // baseURL: 'http://49.173.95.241:14242/api',
-  baseURL: 'http://127.0.0.1:8000/api',
-  // baseURL: 'https://j7e103.p.ssafy.io/api',
+  // baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://j7e103.p.ssafy.io/api',
   // baseURL: 'http://49.173.95.241:14242/api',
   // baseURL: 'http://127.0.0.1:8000/api',
   // baseURL: 'https://j7e103.p.ssafy.io/api',
